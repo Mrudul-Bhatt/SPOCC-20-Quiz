c484cfa3d7a4e78c98d349e9667410a53dac7865
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2020-08-11 16:09:48 +0530

    added user auth2

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,6 @@
 module.exports = {
 	emailRegex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-	baseUrl: 'http://localhost:5000',
+	baseUrl: 'https://examapp-api.herokuapp.com',
 };
 
 //http://localhost:5000
